688ea53ee6d30e2cea1dcb2d7572466297aa12b7
Rafael-Machado032 <rafaelmfds@gmail.com>
2026-06-04 14:51:12 -0300

    a

--- frontend/app/(public)/layout.tsx
+++ frontend/app/(public)/layout.tsx
@@ -9,71 +9,71 @@ import { BuscarFormacaoAction } from "@/api/FormacaoAPI";
 import { BuscarProjetosAction } from "@/api/ProjetoAPI";
 
 const firaCode = Fira_Code({
-    variable: "--font-fira-code",
-    subsets: ["latin"]
+  variable: "--font-fira-code",
+  subsets: ["latin"]
 })
 
 const montserrat = Montserrat({
-    variable: "--font-montserrat-regular",
-    subsets: ["latin"],
+  variable: "--font-montserrat-regular",
+  subsets: ["latin"],
 })
 
 export const metadata: Metadata = { //}Meta Tags steads aqui
+  title: "Portifolio | Rafael Machado",
+  description: "Desenvolvedor Fullstack focado em soluções robustas com Next.js e Laravel",
+  keywords: ["Rafael Machado", "Desenvolvedor", "Fullstack", "Next.js", "Laravel", "Portifolio"],
+  authors: [{ name: "Rafael Machado" }],
+  robots: "index, follow",
+  alternates: {
+    canonical: "https://rafaelmachadodev.vercel.app",
+  },
+  // Configurações de Compartilhamento (Facebook, WhatsApp, LinkedIn)
+  openGraph: {
+    type: "website",
+    url: "https://rafaelmachadodev.vercel.app",
     title: "Portifolio | Rafael Machado",
     description: "Desenvolvedor Fullstack focado em soluções robustas com Next.js e Laravel",
-    keywords: ["Rafael Machado", "Desenvolvedor", "Fullstack", "Next.js", "Laravel", "Portifolio"],
-    authors: [{ name: "Rafael Machado" }],
-    robots: "index, follow",
-    alternates: {
-        canonical: "https://rafaelmachadodev.vercel.app",
-    },
-    // Configurações de Compartilhamento (Facebook, WhatsApp, LinkedIn)
-    openGraph: {
-        type: "website",
-        url: "https://rafaelmachadodev.vercel.app",
-        title: "Portifolio | Rafael Machado",
-        description: "Desenvolvedor Fullstack focado em soluções robustas com Next.js e Laravel",
-        images: [
-            {
-                url: "https://rafaelmachadodev.vercel.app/banner.png",
-                width: 1200,
-                height: 630,
-            },
-        ],
-    },
-    // X (Antigo Twitter)
-    twitter: {
-        card: "summary_large_image",
-        title: "Portifolio | Rafael Machado",
-        description: "Desenvolvedor Fullstack focado em soluções robustas com Next.js e Laravel",
-        images: ["https://rafaelmachadodev.vercel.app/banner.png"],
-    },
+    images: [
+      {
+        url: "https://rafaelmachadodev.vercel.app/banner.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  // X (Antigo Twitter)
+  twitter: {
+    card: "summary_large_image",
+    title: "Portifolio | Rafael Machado",
+    description: "Desenvolvedor Fullstack focado em soluções robustas com Next.js e Laravel",
+    images: ["https://rafaelmachadodev.vercel.app/banner.png"],
+  },
 };
 
 export const viewport = { //A cor da barra do mobile com a cor do site
-    themeColor: [
-        { media: "(prefers-color-scheme: light)", color: "#ffffff" },
-        { media: "(prefers-color-scheme: dark)", color: "#1f2937" },
-    ],
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+    { media: "(prefers-color-scheme: dark)", color: "#1f2937" },
+  ],
 };
 
 export default async function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
 
-    const buscarCurriculo = await BuscarCurriculoAction();
-    const buscarFormacao = await BuscarFormacaoAction();
-    const buscarProjeto = await BuscarProjetosAction();
+  const buscarCurriculo = await BuscarCurriculoAction();
+  const buscarFormacao = await BuscarFormacaoAction();
+  const buscarProjeto = await BuscarProjetosAction();
 
-    return (
-        <html lang="pt-BR" className={` ${firaCode.variable} ${montserrat.variable} h-full antialiased`}>
-            <body className="min-h-full flex flex-col" suppressHydrationWarning>
-                <CurriculoProvedor curriculoInicial={buscarCurriculo?.dados}>
-                    <FormacaoProvedor formacaoInicial={buscarFormacao.dados}>
-                        <ProjetoProvedor projetoInicial={buscarProjeto.dados}>
-                            {children}
-                        </ProjetoProvedor>
-                    </FormacaoProvedor>
-                </CurriculoProvedor>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="pt-BR" className={` ${firaCode.variable} ${montserrat.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        <CurriculoProvedor curriculoInicial={buscarCurriculo?.dados}>
+          <FormacaoProvedor formacaoInicial={buscarFormacao.dados}>
+            <ProjetoProvedor projetoInicial={buscarProjeto.dados}>
+              {children}
+            </ProjetoProvedor>
+          </FormacaoProvedor>
+        </CurriculoProvedor>
+      </body>
+    </html>
+  );
 }
